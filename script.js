7353fb6a51504a4970987532b9246d36929c2965
Aniketyadav388 <aniketyadav56851@gmail.com>
2026-05-08 07:55:00 +0530

    weather app files uploaded

---
+++ script.js
@@ -0,0 +1,61 @@
+const apiKey = "436e26c53678cd73844ec713556e4e71";
+
+async function getWeather(){
+
+  const city = document.getElementById("city").value;
+
+  const loading = document.getElementById("loading");
+
+  const weatherBox = document.getElementById("weather");
+
+  loading.style.display = "block";
+
+  weatherBox.style.display = "none";
+
+  try{
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    const data = await response.json();
+
+    if(data.cod == "404"){
+      alert("City not found");
+      loading.style.display = "none";
+      return;
+    }
+
+    document.getElementById("temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+
+    document.getElementById("cityName").innerHTML =
+      data.name;
+
+    document.getElementById("desc").innerHTML =
+      data.weather[0].description;
+
+    document.getElementById("humidity").innerHTML =
+      data.main.humidity + "%";
+
+    document.getElementById("wind").innerHTML =
+      data.wind.speed + " km/h";
+
+    document.getElementById("icon").src =
+      `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    loading.style.display = "none";
+
+    weatherBox.style.display = "block";
+
+  }
+
+  catch(error){
+
+    alert(" problem smajh nhii aa rhi hai");
+
+    loading.style.display = "none";
+
+  }
+
+}
